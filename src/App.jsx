7a1caf8a35a7131b4a1d67f8cec49f248c128250
Vasjan Çupri <vasjancupri04@gmail.com>
2023-01-27 21:23:04 +0100

    added a feature for notes to be saved locally

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Sidebar from './components/Sidebar'
 import Editor from './components/Editor'
 import { data } from './data'
@@ -6,10 +6,15 @@ import Split from 'react-split'
 import { nanoid } from 'nanoid'
 
 export default function App() {
-  const [notes, setNotes] = React.useState([])
-  const [currentNoteId, setCurrentNoteId] = React.useState(
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem('notes')) || []
+  )
+  const [currentNoteId, setCurrentNoteId] = useState(
     (notes[0] && notes[0].id) || ''
   )
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
 
   function createNewNote() {
     const newNote = {
