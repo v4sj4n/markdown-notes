69f966c24450540cc564119606a440eef1c2ce49
Vasjan Çupri <vasjancupri04@gmail.com>
2023-01-27 21:28:59 +0100

    added lazy initialization on the notes array

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { nanoid } from 'nanoid'
 
 export default function App() {
   const [notes, setNotes] = useState(
-    JSON.parse(localStorage.getItem('notes')) || []
+    () => JSON.parse(localStorage.getItem('notes')) || []
   )
   const [currentNoteId, setCurrentNoteId] = useState(
     (notes[0] && notes[0].id) || ''
